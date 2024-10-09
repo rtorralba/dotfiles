d21c415c3bc4d7c574976b6d2e809482f30b00bc
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2024-10-09 02:08:53 +0200

    improve nvim

---
+++ nvim/lua/plugins/neotest-phpunit.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neotest/neotest",
+  lazy = true,
+  dependencies = {
+    ...,
+    "olimorris/neotest-phpunit",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-phpunit")
+      },
+    })
+  end
+}

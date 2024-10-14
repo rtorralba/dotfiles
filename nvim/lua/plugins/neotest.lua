da50d99e3c811543dbd6e5907e0f17bc31758a4e
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2024-10-15 00:35:28 +0200

    add neotest

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -2,14 +2,17 @@ return {
   "nvim-neotest/neotest",
   lazy = true,
   dependencies = {
-    ...,
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
     "olimorris/neotest-phpunit",
   },
   config = function()
     require("neotest").setup({
       adapters = {
-        require("neotest-phpunit")
+        require("neotest-phpunit"),
       },
     })
-  end
+  end,
 }
